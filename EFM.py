2b17b6fe4254b8240d7825f0328658aaabcd7019
Jason Xurong Liang <xurong.liang@uqconnect.edu.au>
2021-12-14 11:50:13 +1000

    Replace primitive arg check with argparse

--- EFM.py
+++ EFM.py
@@ -4,8 +4,8 @@
 #           2. The directory where the experiment results can be saved
 
 import cornac
-import sys
 import os
+import argparse as ap
 
 import numpy as np
 from cornac.data import SentimentModality
@@ -13,15 +13,29 @@ from cornac.eval_methods import RatioSplit
 from common_func import read_file, get_ratings_and_sentiments, test_ratio, validate_ratio
 
 
+parser = ap.ArgumentParser()
+parser.add_argument("-input", type=str, required=True, help="The location of reviews.pickle")
+parser.add_argument("-output", type=str, default="None", help="Dir to output directory; "
+                                                              "if no need then type 'None'")
+parser.add_argument("-save_matrices", type=str, default="yes", help="whether to save X, Y, U1, U2, H1, H2, V matrices "
+                                                                    "to the output dir")
+# r
+parser.add_argument("-r", type=int, required=True, help="The number of explicit factors")
+# r'
+parser.add_argument("-r_prime", type=int, required=True, help="The number of implicit factors")
+parser.add_argument("-name", type=str, required=True, help="Name of the dataset")
+args = parser.parse_args()
+
 seed_num = 1
-if len(sys.argv) != 5:
-    print("python EFM.py path_to_reviews.pickle path_to_save_evaluation_res "
-          "save_resultant_matrices: yes/no dataset_name")
-    exit(1)
+
 path, res_path, save_matrices, dataset_name = \
-    sys.argv[1], sys.argv[2], sys.argv[3].lower(), sys.argv[4]
+    args.input, args.output, args.save_matrices.lower(), args.name
 
 save_matrices = True if save_matrices == "yes" else False
+
+if save_matrices and args.output == "None":
+    print("Please specify the output folder")
+    exit(1)
 
 if not os.path.isfile(path):
     print("Invalid input file path")
@@ -37,17 +51,13 @@ ratings, sentiments = get_ratings_and_sentiments(df)
 # Instantiate a SentimentModality, it makes it convenient to work with sentiment information
 md = SentimentModality(data=sentiments)
 
-# # use 5-CV
-# cv = CrossValidation(data=ratings, n_folds=5, seed=seed_num, sentiment=md,
-#                      verbose=True)
-
 
 split_data = RatioSplit(data=ratings, test_size=test_ratio, val_size=validate_ratio,
                         seed=seed_num, verbose=True, sentiment=md)
 
 # configure efm model
-r = 40
-r_prime = 60
+r = args.r
+r_prime = args.r_prime
 # top-k number
 k = 15
 N = 5
@@ -62,6 +72,7 @@ model = cornac.models.EFM(num_explicit_factors=r, num_latent_factors=r_prime,
                           lambda_u=lambda_u, lambda_h=lambda_h,
                           lambda_v=lambda_v, trainable=True, verbose=True,
                           seed=seed_num)
+print(f"DataSet {dataset_name}: Now training with r = {r}, r' = {r_prime}")
 
 # performance evaluation metrics: ndcg, recall @ 5, 10, 20, 50
 sample_size = [5, 10, 20, 50]
